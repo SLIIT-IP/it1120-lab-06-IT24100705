0f33bb493df30d3b4c5a8761974c8d944552f858
IT24100705 <it24100705@my.sliit.lk>
2024-08-30 19:57:52 +0530

    Added to Lab6Q1

---
+++ Q1/IT24100705Lab6Q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IT24100705Lab6Q1 {
+public static void main(String[] args) {
+	double num,square,squareRoot;
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter a number : ");
+	num = input.nextDouble();
+	square = Math.sqrt(num);
+	squareRoot =Math.pow(num ,2);
+	System.out.println("The square of "+num+" is : "+square);
+	System.out.println("The square root of "+num+" is : "+squareRoot);
+	
+ }
+	
+}
